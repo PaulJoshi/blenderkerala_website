1fbb3635bc044fd4d840469239d173ff465d4d90
Paul Joshi <49583322+PaulJoshi@users.noreply.github.com>
2020-10-07 19:17:40 +0530

    Basic layout added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
 import React from "react"
+import '../styles/index.css'
+import BlenderKeralaLogo from "../assets/blenderkeralalogo.jpg"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  return <div className='container'>
+    <header>
+      <h1><Link  className='title' to=''>BLENDERKERALA</Link></h1>
+      <nav>
+        <ul>
+          <li><Link className='nav-item' to='/features/'>Recent Features</Link></li>
+          <li><Link className='nav-item' to='/request/'>Request Artwork</Link></li>
+          <li><Link className='nav-item' to='/about/'>About</Link></li>
+        </ul>
+      </nav>
+    </header>
+    <div className='home'>
+      <img className='logo-large' src={BlenderKeralaLogo} alt='BlenderKerala logo'/>
+      <p>Join our Community!</p>
+      <ul>
+        <li><img className='social-icon' src='' alt='Instagram'/></li>
+        <li><img className='social-icon' src='' alt='WhatsApp'/></li>
+        <li><img className='social-icon' src='' alt='Discord'/></li>
+      </ul>
+      <p>BlenderKerala is a community of Blender Artists based in Kerala, India.</p>
+    </div>
+  </div>
 }
